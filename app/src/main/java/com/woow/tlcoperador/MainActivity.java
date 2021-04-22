96fdf1857f30bd27fde4b1b6ff88734f9c0aed51
Guille <guillermo.maestre@hotmail.com>
2021-04-22 01:43:11 -0300

    - Conexion con firebase y login.

--- app/src/main/java/com/woow/tlcoperador/MainActivity.java
+++ app/src/main/java/com/woow/tlcoperador/MainActivity.java
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);

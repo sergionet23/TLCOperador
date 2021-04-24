826b631917c6b30e75700319115845681d2cbfce
sergionet23 <sergionet23@gmail.com>
2021-04-24 00:39:55 -0300

    Link Monedero corregido

--- app/src/main/java/com/woow/tlcoperador/ui/Monedero/MonederoFragment.java
+++ app/src/main/java/com/woow/tlcoperador/ui/Monedero/MonederoFragment.java
@@ -1,18 +1,14 @@
 package com.woow.tlcoperador.ui.Monedero;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
